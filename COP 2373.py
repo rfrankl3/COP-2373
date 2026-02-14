eb0bc3b6546a240ce3fb3238196185603a1d0dd2
Robyn Franklin <rfrankl3@student.scf.edu>
2026-02-14 13:36:18 -0500

    Change message 2/14/2026

--- COP 2373.py
+++ COP 2373.py
@@ -3,7 +3,7 @@ def sell_tickets():
     total_buyers = 0
     while remaining_tickets > 0:
         print("\nTickets remaining:", remaining_tickets)
-        tickets = int(input("Enter number of tickets to buy (1-4): "))
+        tickets = int(input("How many tickets would you like to purchase today? (1-4): "))
         if tickets >= 1 and tickets <= 4 and tickets <= remaining_tickets:
             remaining_tickets = remaining_tickets - tickets
             total_buyers = total_buyers + 1
